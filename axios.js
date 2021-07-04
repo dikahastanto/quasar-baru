db081417cbc635d19b68da7db9cca6d31dc67ae6
dikahastanto <dika.15411036@student.ubl.ac.id>
2021-07-04 18:27:39 +0700

    aa

--- axios.js
+++ axios.js
@@ -1,10 +1,14 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
-const axiosInstance = axios.create({ baseURL: 'http://localhost:3000' })
+// penjelasan lebih lengkap baca disini ya
+// https://quasar.dev/quasar-cli/ajax-requests#introduction
+const baseURL = 'http://localhost:3000'
+const axiosInstance = axios.create({ baseURL: baseURL })
 
 export default boot(({ app }) => {
   app.config.globalProperties.$axios = axiosInstance
+  app.config.globalProperties.$baseImageURL = baseURL
 })
 
 export { axios }
